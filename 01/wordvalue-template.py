af0d35167b0436d3ffb2c52332c67ee7238bbe8b
anubhavsinha98 <anubhavsinha98@gmail.com>
2018-11-23 05:41:00 +0530

    01

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -1,18 +1,34 @@
 from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
-    """Load dictionary into a list and return list"""
-    pass
+	l=[]
+	f = open(DICTIONARY,'r')
+	for x in f:
+		l.append(x[:len(x)-2].upper())
+	return l
 
-def calc_word_value():
-    """Calculate the value of the word entered into function
-    using imported constant mapping LETTER_SCORES"""
-    pass
+def calc_word_value(l):
+	p = []
+	#print(l)
+	for x in l:
+		value = 0
+		for i in x:
+			try:
+				value+=LETTER_SCORES[i]
+			except KeyError:
+				continue
+		p.append([x,value])
+	return p
 
-def max_word_value():
-    """Calculate the word with the max value, can receive a list
-    of words as arg, if none provided uses default DICTIONARY"""
-    pass
+def max_word_value(p):
+	index = 0
+	maxi = 0
+	for i in range(len(p)):
+		if maxi < p[i][1]:
+			maxi = p[i][1]
+			index = i
+	print(p[index][0])		
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+	max_word_value(calc_word_value(load_words()));
+	# run unittests to validate
